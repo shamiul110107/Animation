41d0a510700672c0d23de5f323ccb67350ae47aa
Steve JobsOne <sami@mobioapps-mac-mini-2.local>
2018-02-19 11:45:55 +0600

    Initial Commit

---
+++ AnimationCore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationCore
+//
+//  Created by Sami on 2/19/18.
+//  Copyright © 2018 Sami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AnimationCore/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AnimationCore
+//
+//  Created by Sami on 2/19/18.
+//  Copyright © 2018 Sami. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
